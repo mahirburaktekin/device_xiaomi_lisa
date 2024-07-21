2a5b1afaa92634dba04cb790f95e7bf1d7e61aa5
mahirburaktekin <84406060+mahirburaktekin@users.noreply.github.com>
2024-07-21 15:53:23 +0300

    Update and rename blaze_lisa.mk to lineage_lisa.mk

---
+++ lineage_lisa.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2022 PixelOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lisa device
+$(call inherit-product, device/xiaomi/lisa/device.mk)
+
+# Inherit some common Project-Blaze stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+PRODUCT_ENABLE_UFFD_GC := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+PRODUCT_ENABLE_UFFD_GC := true
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := lisa
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := lineage_lisa
+PRODUCT_MODEL := Xiaomi 11 Lite NE
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
